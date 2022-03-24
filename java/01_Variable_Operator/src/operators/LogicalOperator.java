4f6d04c5b4c785cb7cb35dba6ab01b12b0957f7a
devimpalla <90491326+devimpalla@users.noreply.github.com>
2022-03-24 14:10:42 +0900

    java2

---
+++ java/01_Variable_Operator/src/operators/LogicalOperator.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class LogicalOperator {
+
+	public static void main(String[] args) {
+		System.out.println("논리연산자 => 리턴은 boolean!!!");
+		System.out.println("!,$$,||,");
+		
+		System.out.println(true && false);
+		System.out.println(false || true);
+		System.out.println(!false);
+		
+		
+	}
+
+}
---
+++ java/01_Variable_Operator/src/operators/산술연산자.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class 산술연산자 {
+// 한글로 클래스명 가능하나 컴파일 할 때 인코딩 변환과정이 한번더 진행되므로 속도가 느려짐
+	public static void main(String[] args) {
+		// 라인 하나 지우는거 ctrl + d
+		// 산술연산자는 숫자연산과 관련된 연산자
+		System.out.println("사칙연산자: +,-,*,/");
+		System.out.println("나머지연산자 : %");
+		System.out.println("증감연산자 : ++,--");
+		
+		// Assignment Operator 대입연산자
+		System.out.println("=,+=,-=,*=,/=,%=");
+		
+	}
+
+}
